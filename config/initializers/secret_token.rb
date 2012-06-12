6eccd1853db5574a9e7ec92bf66265aacf2a526b
ngdeguzman@hotmail.com <nicholaideguzman@neurology-imac.dhcp.swmed.org>
2012-06-12 16:57:05 -0500

    devise working

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Devising::Application.config.secret_token = 'bbe725ef50b6057bff0c2e7aaca2e5db1aaa6b232bf18cdd8f849d49fb9ec6ebbdc8deda2d7c905837a9d20381e72164e840132f27cbe77371970d32f0037333'
